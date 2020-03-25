7d1356ffb9d569f0edb49051fe23cbd0a4a6c488
Stefan Stroe <stroestefan97@gmail.com>
2020-03-25 13:53:16 +0000

    remove webpack config override

---
+++ precache-manifest.d6b900b3b99ca9c6cf639ffc433c34c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2808c5aa8685d9d12f18ff053b9db70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f2ba0ef0794d594d76",
+    "url": "/static/css/2.5b0da294.chunk.css"
+  },
+  {
+    "revision": "d731cc292ca218ab0f99",
+    "url": "/static/css/main.ffb0518e.chunk.css"
+  },
+  {
+    "revision": "a4f2ba0ef0794d594d76",
+    "url": "/static/js/2.41fa4d9c.chunk.js"
+  },
+  {
+    "revision": "6994bf86f6225aa1dd0f2d756f3add43",
+    "url": "/static/js/2.41fa4d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d731cc292ca218ab0f99",
+    "url": "/static/js/main.7cb47735.chunk.js"
+  },
+  {
+    "revision": "85e6a98b86ef3993b97a",
+    "url": "/static/js/runtime-main.5de74a9c.js"
+  }
+]);
